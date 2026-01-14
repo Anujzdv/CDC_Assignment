ca238d1b49d7270b86a84a59fe90f35d72279889
ANUJ KUMAR <cse23219@glbitm.ac.in>
2026-01-14 22:37:05 +0530

    Time: 404 ms (20.85%) | Memory: 263.3 MB (9.34%) - LeetSync

---
+++ 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int row, col;
+    vector<vector<int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    bool canCross(int day, vector<vector<int>>& cells) {
+        vector<vector<int>> grid(row, vector<int>(col, 0));
+
+        for (int i = 0; i < day; i++) {
+            int r = cells[i][0] - 1;
+            int c = cells[i][1] - 1;
+            grid[r][c] = 1; // water
+        }
+
+        queue<pair<int,int>> q;
+        vector<vector<int>> vis(row, vector<int>(col, 0));
+
+        for (int c = 0; c < col; c++) {
+            if (grid[0][c] == 0) {
+                q.push({0, c});
+                vis[0][c] = 1;
+            }
+        }
+
+        while (!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+
+            if (r == row - 1) return true;
+
+            for (auto &d : dir) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr >= 0 && nc >= 0 && nr < row && nc < col &&
+                    !vis[nr][nc] && grid[nr][nc] == 0) {
+                    vis[nr][nc] = 1;
+                    q.push({nr, nc});
+                }
+            }
+        }
+
+        return false;
+    }
+
+    int latestDayToCross(int r, int c, vector<vector<int>>& cells) {
+        row = r;
+        col = c;
+
+        int low = 1, high = cells.size(), ans = 0;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (canCross(mid, cells)) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
