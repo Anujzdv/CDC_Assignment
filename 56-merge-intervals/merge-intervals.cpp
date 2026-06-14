05b2906563ef05b46dded079a846ecaf90875e61
ANUJ KUMAR <cse23219@glbitm.ac.in>
2026-06-14 22:40:10 +0530

    Time: 7 ms (39.83%) | Memory: 23.9 MB (59.02%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& itr) {
+        vector<vector<int>>ans;
+        sort(itr.begin(), itr.end());
+        ans.push_back(itr[0]);
+        int n = itr.size();
+        for(int i = 1; i< n; i++){
+            int prvend = ans.back()[1];
+            int currst = itr[i][0];
+            int currend = itr[i][1];
+            if(prvend >= currst){
+                int lidx = ans.size();
+                ans[lidx -1][1] = max(currend, prvend);
+            }else{
+                ans.push_back(itr[i]);
+            }
+        }
+        return ans;
+    }
+};
