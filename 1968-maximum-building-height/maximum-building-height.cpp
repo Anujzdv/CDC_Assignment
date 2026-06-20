9737000cb85ffef68e1570fdf81763078f190c7e
ANUJ KUMAR <cse23219@glbitm.ac.in>
2026-06-21 01:48:31 +0530

    Time: 96 ms (13.70%) | Memory: 106.7 MB (38.36%) - LeetSync

---
+++ 1968-maximum-building-height/maximum-building-height.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int maxBuilding(int n, vector<vector<int>>& restrictions) {
+        
+        restrictions.push_back({1, 0});
+        sort(restrictions.begin(), restrictions.end());
+
+        int m = restrictions.size();
+
+        // Left -> Right
+        for (int i = 1; i < m; i++) {
+            int dist = restrictions[i][0] - restrictions[i - 1][0];
+            restrictions[i][1] =
+                min(restrictions[i][1],
+                    restrictions[i - 1][1] + dist);
+        }
+
+        // Right -> Left
+        for (int i = m - 2; i >= 0; i--) {
+            int dist = restrictions[i + 1][0] - restrictions[i][0];
+            restrictions[i][1] =
+                min(restrictions[i][1],
+                    restrictions[i + 1][1] + dist);
+        }
+
+        long long ans = 0;
+
+        for (int i = 1; i < m; i++) {
+
+            long long id1 = restrictions[i - 1][0];
+            long long h1  = restrictions[i - 1][1];
+
+            long long id2 = restrictions[i][0];
+            long long h2  = restrictions[i][1];
+
+            long long dist = id2 - id1;
+
+            ans = max(ans, (h1 + h2 + dist) / 2);
+        }
+
+        long long lastId = restrictions.back()[0];
+        long long lastHeight = restrictions.back()[1];
+
+        ans = max(ans, lastHeight + (n - lastId));
+
+        return (int)ans;
+    }
+};
